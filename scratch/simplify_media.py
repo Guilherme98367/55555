0f1aed4232b9c594409823ada2e969da1b2663f9
Gui@201301 <heytor.gui300@gmail.com>
2026-05-28 09:42:22 -0300

    Primeiro envio da pasta 555

---
+++ scratch/simplify_media.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eeef00e21eadb36f56e99e7427e58727cf3f0edd35c93ca62d9181ad285aee2
+size 8463
---
+++ scratch/validate_paths.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bbb1f9a712217b9f64c3df31c2cdd18424d38a6ad233192aad21a8a4124d009
+size 3866
